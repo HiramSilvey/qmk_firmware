eb05ab829a5cfdff0d3f4f73176865b4aca2bc98
Hiram Silvey <blujay270@gmail.com>
2020-06-12 02:16:30 -0400

    massive overhaul and simplification of the keymap

--- keyboards/ergodox_ez/keymaps/hiram-silvey/keymap.c
+++ keyboards/ergodox_ez/keymaps/hiram-silvey/keymap.c
@@ -1,14 +1,10 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 
-#define PERMISSIVE_HOLD
-
-#define BASE     0 // default layer
-#define NUM      1 // numpad
-#define NAV      2 // navigation
-#define FN       3 // function key numpad
-#define MDIA     4 // media
-#define GAME     5 // gaminzg
+#define BASE 0
+#define NUM  1
+#define FN   2
+#define GAME 3
 
 enum custom_keycodes {
 #ifdef ORYX_CONFIGURATOR
@@ -17,133 +13,169 @@ enum custom_keycodes {
 	EPRM = SAFE_RANGE,
 #endif
 	VRSN,
-  LCLR,
-	E_VSLZ,
-	E_UNDO,
-	E_REDO,
-	T_PREV,
-	T_NEXT,
-	T_PRFX,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
+/* BASE layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * | Esc    |  @   |  !   |  =   |  (   |  &   |  ^   |           |   `  |   *  |   )  |   [  |   ]  |   #  |    Del |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | Tab    |  '   |  ,   |  .   |  P   |  Y   |  /   |           |   \  |   F  |   G  |   C  |   R  |   L  |  Enter |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | MO(L1) |  A   |  O   |  E   |  U   |  I   |------|           |------|   D  |   H  |   T  |   N  |   S  |    -   |
+ * |--------+------+------+------+------+------|  %   |           |   $  |------+------+------+------+------+--------|
+ * | Lshift |  ;   |  Q   |  J   |  K   |  X   |      |           |      |   B  |   M  |   W  |   V  |   Z  | RShift |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |TG(L3)| Home | PgDn | PgUp | End  |                                       | Left |  Up  | Down | Right|CpsLck|
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |VolDn |VolUp |       |PTrack|NTrack|
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      | Mute |       | Play |      |      |
+ *                                 |Space | Left |------|       |------|Right |Back- |
+ *                                 |      | Ctrl | LCmd |       | RCmd |  Alt | space|
+ *                                 `--------------------'       `--------------------'
+ */
 [BASE] = LAYOUT_ergodox(
-	// left hand
-	KC_ESC, KC_AT, KC_EXLM, KC_EQL, KC_LPRN, KC_AMPR, KC_GRV,
-	KC_LALT, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_PERC,
-  KC_LCTL, KC_A, KC_O, KC_E, KC_U, KC_I,
-	KC_LSFT, KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_CAPS,
-	E_VSLZ, MO(NUM), MO(NAV), MO(FN), MO(MDIA),
-	LCTL(KC_LALT), KC_MEH,
-  TG(GAME),
-	KC_SPC, KC_TAB, KC_LGUI,
-	// right hand
-	KC_CIRC, KC_ASTR, KC_RPRN, KC_LBRC, KC_RBRC, KC_HASH, KC_BSLS,
-	KC_DLR, KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH,
-	KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS,
-	KC_DEL, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RSFT,
-	T_PREV, T_NEXT, E_UNDO, E_REDO, T_PRFX,
-	KC_HYPR, RGUI(KC_RSFT),
-	LCLR,
-	KC_RGUI, KC_ENT, KC_BSPC
+	// Left hand
+  KC_ESC,   KC_AT,   KC_EXLM, KC_EQL,  KC_LPRN, KC_AMPR, KC_CIRC,
+	KC_TAB,   KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_SLSH,
+  MO(NUM),  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,
+	KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_PERC,
+	TG(GAME), KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+	                                              KC_VOLD, KC_VOLU,
+                                                         KC_MUTE,
+                         	             KC_SPC,  KC_LCTL, KC_LGUI,
+	// Right hand
+	KC_GRV,  KC_ASTR, KC_RPRN, KC_LBRC, KC_RBRC, KC_HASH, KC_DEL,
+	KC_BSLS, KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_ENT,
+	         KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+	KC_DLR,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
+	                  KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_CAPS,
+	KC_MPRV, KC_MNXT,
+	KC_MPLY,
+	KC_RGUI, KC_RALT, KC_BSPC
 ),
+/* NUM layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |   *  |   7  |   8  |   9  |   /  |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |MO(L2)|      |------|           |------|   +  |   4  |   5  |   6  |   -  |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |   0  |   1  |   2  |   3  |   .  |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
 [NUM] = LAYOUT_ergodox(
-	// left hand
+	// Left hand
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(FN),  KC_TRNS,
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-	KC_TRNS, KC_TRNS,
-	KC_TRNS,
-	KC_TRNS, KC_TRNS, KC_TRNS,
-	// right hand
+	                                             KC_TRNS, KC_TRNS,
+	                                                      KC_TRNS,
+	                                    KC_TRNS, KC_TRNS, KC_TRNS,
+	// Right hand
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-	KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_TRNS, KC_TRNS,
-	KC_DLR,  KC_4,    KC_5,    KC_6,    KC_PAST, KC_TRNS,
-	KC_TRNS, KC_PERC, KC_1,    KC_2,    KC_3,    KC_PSLS, KC_TRNS,
-	KC_0,    KC_PMNS, KC_PPLS, KC_TRNS, KC_TRNS,
+	KC_TRNS, KC_PAST, KC_7,    KC_8,    KC_9,    KC_PSLS, KC_TRNS,
+	         KC_PPLS, KC_4,    KC_5,    KC_6,    KC_PMNS, KC_TRNS,
+	KC_TRNS, KC_0,    KC_1,    KC_2,    KC_3,    KC_PDOT, KC_TRNS,
+	                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 	KC_TRNS, KC_TRNS,
 	KC_TRNS,
 	KC_TRNS, KC_TRNS, KC_TRNS
 ),
-[NAV] = LAYOUT_ergodox(
-  // left hand
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS,
-  KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS,
-  // right hand
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_PGUP, KC_UP,   KC_HOME, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_PGDN, KC_TRNS, KC_END, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS,
-  KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS
-),
+/* FN layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |      |      |      |      |      |      |           |      |      |  F10 |  F11 |  F12 |      |  EPRM  |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |   F7 |   F8 |   F9 |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |------|           |------|      |   F4 |   F5 |   F6 |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |   F1 |   F2 |   F3 |      |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
 [FN] = LAYOUT_ergodox(
-	// left hand
-  EPRM,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+	// Left hand
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS,
-  KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS,
-	// right hand
-	KC_TRNS, KC_TRNS, KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS,
+                                               KC_TRNS, KC_TRNS,
+                                                        KC_TRNS,
+                                      KC_TRNS, KC_TRNS, KC_TRNS,
+	// Right hand
+	KC_TRNS, KC_TRNS, KC_F10,  KC_F11,  KC_F12,  KC_TRNS, EPRM,
 	KC_TRNS, KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_TRNS, KC_TRNS,
-	KC_TRNS, KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_TRNS,
+	         KC_TRNS, KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_TRNS,
 	KC_TRNS, KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_TRNS, KC_TRNS,
-	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+	                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 	KC_TRNS, KC_TRNS,
 	KC_TRNS,
 	KC_TRNS, KC_TRNS, KC_TRNS
 ),
-[MDIA] = LAYOUT_ergodox(
-  // left hand
-  VRSN,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS,
-  KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS,
-  // right hand
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_BRID, KC_BRIU, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_SLEP, KC_WAKE, KC_PWR,  KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS,
-  KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS
-),
+/* GAME layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |  1   |  2   |  3   |  4   |  5   |      |           |      |   6  |   7  |   8  |   9  |   0  |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |  Q   |  W   |  E   |  R   |  T   |      |           |      |   Y  |   U  |   I  |   O  |   P  |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | LCtrl  |  A   |  S   |  D   |  F   |  G   |------|           |------|   H  |   J  |   K  |   L  |   ;  |        |
+ * |--------+------+------+------+------+------| LAlt |           |      |------+------+------+------+------+--------|
+ * |        |  Z   |  X   |  C   |  V   |  B   |      |           |      |   N  |   M  |   ,  |   .  |   /  |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |  0   |  6   |  7   |  8   |  9   |                                       |      |      |      |      |      |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
 [GAME] = LAYOUT_ergodox(
-  // left hand
-  KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_TRNS,
-  KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_TRNS,
-  KC_TRNS, KC_A, KC_S, KC_D, KC_F, KC_G,
-  KC_TRNS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_TRNS,
-  KC_6, KC_7, KC_8, KC_9, KC_0,
-  KC_TRNS, KC_TRNS,
-  KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS,
-  // right hand
-  KC_TRNS, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
-  KC_TRNS, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_TRNS,
-  KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_TRNS,
-  KC_TRNS, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  // Left hand
+  KC_TRNS, KC_1, KC_2, KC_3, KC_4,    KC_5,    KC_TRNS,
+  KC_TRNS, KC_Q, KC_W, KC_E, KC_R,    KC_T,    KC_TRNS,
+  KC_LCTL, KC_A, KC_S, KC_D, KC_F,    KC_G,
+  KC_TRNS, KC_Z, KC_X, KC_C, KC_V,    KC_B,    KC_LALT,
+  KC_0,    KC_6, KC_7, KC_8, KC_9,
+                                      KC_TRNS, KC_TRNS,
+                                               KC_TRNS,
+                             KC_TRNS, KC_TRNS, KC_TRNS,
+  // Right hand
+  KC_TRNS, KC_6, KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
+  KC_TRNS, KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    KC_TRNS,
+           KC_H, KC_J,    KC_K,    KC_L,    KC_SCLN, KC_TRNS,
+  KC_TRNS, KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS,
   KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS
@@ -159,27 +191,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			case VRSN:
 				SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
 				return false;
-      case LCLR:
-        layer_clear();
-        return false;
-			case E_UNDO:
-				SEND_STRING(SS_LCTL("/"));
-				break;
-			case E_REDO:
-				SEND_STRING(SS_LALT(SS_LSFT("-")));
-				break;
-			case E_VSLZ:
-				SEND_STRING(SS_LCTL("u")"u");
-				break;
-			case T_PREV:
-				SEND_STRING(SS_TAP(X_F9)"p");
-				break;
-			case T_NEXT:
-				SEND_STRING(SS_TAP(X_F9)"n");
-				break;
-			case T_PRFX:
-				SEND_STRING(SS_TAP(X_F9));
-				break;
 		}
 	}
 	return true;
