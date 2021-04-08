f8a4cc047664293bbf3f8ac794ac7e816dde08fc
Hiram Silvey <blujay270@gmail.com>
2021-04-08 09:34:19 -0400

    Reconciled mismatched diagram and code, keyboard was missing % symbol

--- keyboards/ergodox_ez/keymaps/hiram-silvey/keymap.c
+++ keyboards/ergodox_ez/keymaps/hiram-silvey/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_CIRC, KC_AMPR, KC_RPRN, KC_LBRC, KC_RBRC, KC_EQL,  KC_DEL,
 	KC_BSLS, KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_ENT,
 	         KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
-	KC_GRV,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
+	KC_PERC, KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
 	                  KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_PSCR,
 	KC_MPRV, KC_MNXT,
 	KC_MPLY,
